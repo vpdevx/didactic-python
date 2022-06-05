58327948806c68a278ac6a0927e56173714fea13
viniciuscarvalhopires <vinicpires03@gmail.com>
2022-06-05 12:34:26 +0000

    RandomComputerPlayer vs AI

--- freecodecamp-projects/tic-tac-toe-AI/game.py
+++ freecodecamp-projects/tic-tac-toe-AI/game.py
@@ -105,14 +105,26 @@ def play(game, x_player, o_player, print_game=True):
                 return letter
             #after we made our move we need to alternate letters 
             letter = 'O' if letter == 'X' else 'X' 
-
-        time.sleep(.8)
+        if print_game:
+            time.sleep(.8)
     if print_game:
         print('It\'s a tie')
 
 if __name__ == '__main__':
-    x_player = HumanPlayer('X')
-    o_player = GeniusComputerPlayer('O')
-    t = TicTacToe()
-    play(t, x_player, o_player, print_game=True)
+    x_wins = 0
+    o_wins = 0
+    ties = 0
+    for _ in range(1000):
+        x_player = RandomComputerPlayer('X')
+        o_player = GeniusComputerPlayer('O')
+        t = TicTacToe()
+        result = play(t, x_player, o_player, print_game=False)
+        if result == 'X':
+            x_wins += 1
+        elif result == 'O':
+            o_wins += 1
+        else:
+            ties += 1
+    
+    print(f"After 1000 interations, we see {x_wins} X wins, {o_wins} O wins, {ties} ties.")
         
